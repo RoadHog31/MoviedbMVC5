6d54d60e8da0e28d8a49278cb50dbfd7ecea7282
SPHyrule <stphnpino1@aol.com>
2020-07-02 12:51:48 +0100

    Controller and view added. Bootswatch theme also applied.

---
+++ Models/Movie.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MoviedbMVC5.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
