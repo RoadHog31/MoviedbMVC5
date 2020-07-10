1676012aad6c92adfe45d809d7ebbd9f22bd626e
SPHyrule <stphnpino1@aol.com>
2020-07-10 11:31:53 +0100

    Migrations added

---
+++ Migrations/202007101027278_updated.cs
@@ -0,0 +1,20 @@
+﻿namespace MoviedbMVC5.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updated : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Movies", "Title", c => c.String(nullable: false));
+            AlterColumn("dbo.Movies", "Genre", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Movies", "Genre", c => c.String());
+            AlterColumn("dbo.Movies", "Title", c => c.String());
+        }
+    }
+}
